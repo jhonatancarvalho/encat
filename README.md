0b6ea4447eab7826d2ed7accd5b33aaeb02e2b33
Jhonatan Carvalho <jhonatan.carvalho92@gmail.com>
2018-02-21 21:12:49 -0300

    Primeiro commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Webservice para criação de enquetes
---
+++ src/main/java/br/com/jhonatan/encat/EncatApplication.java
@@ -0,0 +1,12 @@
+package br.com.jhonatan.encat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncatApplication.class, args);
+	}
+}
